1fd8845ef9b441964b5490c297b9c51a41d6e0f2
netrunnerd <41193007+netrunnerd@users.noreply.github.com>
2019-08-13 05:38:26 -0400

    Add a knexfile

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/cars.db3'
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    seeds: {
+      directory: './src/database/seeds'
+    }
+  }
+};
