72aea1551370d32dfe186dafb1e8c4c74e490608
netrunnerd <41193007+netrunnerd@users.noreply.github.com>
2019-08-13 11:36:56 -0400

    POST /api/cars

--- src/controllers/cars.js
+++ src/controllers/cars.js
@@ -49,3 +49,40 @@ exports.one = async (req, res) => {
     });
   }
 };
+
+exports.submit = async (req, res) => {
+  let car = req.body;
+  const length = Object.keys(car).length;
+  const { make, model, mileage, vin } = car;
+
+  if (length === 0) {
+    return res.status(400).json({
+      message: "Missing car data.",
+      success: false
+    });
+  }
+
+  if (length > 0 && !make || !model || !mileage || !vin) {
+    return res.status(400).json({
+      message: "Missing required make, model, mileage or VIN.",
+      success: false
+    });
+  }
+
+  try {
+    car = await m.new(car);
+
+    if (car) {
+      return res.json({
+        car,
+        success: true
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      error,
+      message: "Car could not be saved.",
+      success: false
+    });
+  }
+};
--- src/routes/cars.js
+++ src/routes/cars.js
@@ -4,5 +4,6 @@ const cars = require('../controllers/cars');
 
 router.get('/', cars.all);
 router.get('/:id', cars.one);
+router.post('/', cars.submit);
 
 module.exports = router;
