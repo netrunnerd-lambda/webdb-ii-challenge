836ed7963f5c357652f88931a85fa199fb6c85ab
netrunnerd <41193007+netrunnerd@users.noreply.github.com>
2019-08-13 07:37:36 -0400

    GET /api/cars/:id

--- src/controllers/cars.js
+++ src/controllers/cars.js
@@ -22,4 +22,30 @@ exports.all = async (req, res) => {
       success: false
     });
   }
-}
+};
+
+exports.one = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const [ inventory ] = await m.list(id);
+    
+    if (!inventory) {
+      return res.status(404).json({
+        message: `Car ${id} does not exist.`,
+        success: false
+      });
+    }
+
+    return res.json({
+      inventory,
+      success: true
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error,
+      message: "Car could not be retrieved.",
+      success: false
+    });
+  }
+};
--- src/routes/cars.js
+++ src/routes/cars.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 const cars = require('../controllers/cars');
 
 router.get('/', cars.all);
+router.get('/:id', cars.one);
 
 module.exports = router;
