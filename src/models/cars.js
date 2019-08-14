aaf7c8a32572c7e23b7fd4163248079616f0d9f4
netrunnerd <41193007+netrunnerd@users.noreply.github.com>
2019-08-13 21:14:41 -0400

    Update cars model

--- src/models/cars.js
+++ src/models/cars.js
@@ -4,4 +4,9 @@ const tb = 'cars';
 exports.list = id => id ? db(tb).where({ id }) : db(tb);
 exports.new = data => db(tb).insert(data).then(([ id ]) => this.list(id));
 exports.remove = id => db(tb).where({ id }).del();
-exports.update = (id, data) => db(tb).where({ id }).update(data);
+exports.update = (id, data) => {
+  return db(tb)
+          .where({ id })
+          .update(data)
+          .then(() => this.list(id));
+};
